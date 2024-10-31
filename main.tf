f0fe8cb2d56f4383d38fca3d375f1e0d76200191
dragao-chines <marratti@outlook.com>
2024-10-31 00:11:21 -0300

    merge de arquivos

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
 terraform {
   required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.7.0"
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.73.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
-  
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "desigua"
+      managed-by = "terraform"
+    }
+  }
+
 }
