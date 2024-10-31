3cac544ecbb0decd7d04ec6ac6e7e743c5ea640f
dragao-chines <marratti@outlook.com>
2024-10-31 00:06:55 -0300

    merge de arquivos

--- bucket.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_s3_bucket" "s3-desigua-1" {
-  bucket = "desigua-s3-terraform-1"
-}
--- main.tf
+++ main.tf
@@ -1,39 +1,13 @@
 terraform {
-<<<<<<< HEAD
-    required_version = "~>1.9.0"
-
-    required_providers {
-        aws = {
-            source = "hashcorp/aws"
-        }
-
-           
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.7.0"
     }
-
-}
-
-provider "aws" {
-    region = "us-east-1"
-
-    default_tags {
-        tags = {
-            owner = "desigua"
-            managed-by = "terraform"
-        }
-    }
-=======
-    required_version = ">=1.0.0"
-
-    required_providers {
-      azurerm = {
-        source = "hashicorp/azurerm"
-        version = ">=3.48.0"
-      }
-    }
+  }
 }
 
 provider "azurerm" {
-    features {}
-
->>>>>>> fb518fb (estudos azurerm - storage account)
+  features {}
+  
 }
